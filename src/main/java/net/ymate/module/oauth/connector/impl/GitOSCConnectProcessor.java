702339c2eb83d794b80e16b398237404c15e0a89
suninformation <suninformation@163.com>
2017-05-26 18:27:41 +0800

    新增GitOSC码云OAuth2授权登录服务实现

---
+++ src/main/java/net/ymate/module/oauth/connector/impl/GitOSCConnectProcessor.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2007-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.ymate.module.oauth.connector.impl;
+
+import com.alibaba.fastjson.JSONObject;
+import net.ymate.framework.commons.HttpClientHelper;
+import net.ymate.framework.commons.IHttpResponse;
+import net.ymate.module.oauth.connector.AbstractOAuthConnectProcessor;
+import net.ymate.module.oauth.connector.OAuthConnectUser;
+import net.ymate.module.oauth.connector.annotation.OAuthConnectProcessor;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * @author 刘镇 (suninformation@163.com) on 17/5/26 下午5:27
+ * @version 1.0
+ */
+@OAuthConnectProcessor("gitosc")
+public class GitOSCConnectProcessor extends AbstractOAuthConnectProcessor {
+
+    private static final String __CONNECT_URL = "http://git.oschina.net/oauth/authorize?";
+
+    private static final String __TOKEN_URL = "http://git.oschina.net/oauth/token?";
+
+    private static final String __USERINFO_URL = "https://git.oschina.net/api/v5/user?access_token=";
+
+    public GitOSCConnectProcessor() {
+        super();
+        this.__doSetNeedRedirectUri(true);
+    }
+
+    public String getAuthorizeUrl(String state) {
+        return __CONNECT_URL + __doBuildAuthzUrl("user_info", state, true);
+    }
+
+    public OAuthConnectUser getConnectUser(String code) throws Exception {
+        OAuthConnectUser _connectUser = __doGetAccessToken(code, __TOKEN_URL);
+        if (_connectUser != null) {
+            if (StringUtils.isNotBlank(_connectUser.getAccessToken())) {
+                IHttpResponse _response = HttpClientHelper.create().get(__USERINFO_URL.concat(_connectUser.getAccessToken()));
+                JSONObject _result = __doParseConnectResponseBody(_response);
+                if (_result != null) {
+                    _connectUser.setOpenId(_result.getString("id"))
+                            .setNickName(_result.getString("login"))
+                            .setPhotoUrl(_result.getString("avatar_url"));
+                }
+            }
+        }
+        return _connectUser;
+    }
+}
